fd64f741746d3609a0639b87ce6d43a2a88d55ae
Keerthana Ramakrishnan <keerthana.r@thoughtworks.com>
2024-03-18 19:46:21 +0530

    Use env variable for postgres url

--- src/database.js
+++ src/database.js
@@ -1,17 +1,15 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize('postgres://default:XAx8mr2dhYSB@ep-broad-fire-a1nkj6dz-pooler.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require' ,{
+const sequelize = new Sequelize(process.env.POSTGRES_URL,{
     host:'ep-broad-fire-a1nkj6dz-pooler.ap-southeast-1.aws.neon.tech',
     dialect: 'postgres'
 });
 
 function testConnection() {
-    console.log("testig connection")
     sequelize.authenticate()
         .then(() => console.log('Connection to the database has been established successfully.'))
         .catch((error) =>console.error('Unable to connect to the database:', error))
-    console.log("testig connection started")
 }
 
 module.exports = { sequelize, testConnection };

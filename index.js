513c7aa58c7f9269b7fc4327f5f59b98620203a3
Keerthana Ramakrishnan <keerthana.r@thoughtworks.com>
2024-03-18 19:59:44 +0530

    Remove unwanted comments

--- index.js
+++ index.js
@@ -20,45 +20,6 @@ app.use('/api-docs', swagger.serve, swagger.setup);
 app.use('/api/v1', organizationRoutes);
 app.use('/api/v1', itemRoutes);
 app.use('/api/v1', pricingRoutes);
-
-/**
- * @swagger
- * /books:
- *   get:
- *     description: Get all books
- *     responses:
- *       200:
- *         description: Success
- *
- */
-app.get('/books', (req, res) => {
-    res.send([
-        {
-            id: 1,
-            title: "Harry Potter",
-        }
-    ])
-});
-
-/**
- * @swagger
- * /books:
- *   post:
- *     description: Get all books
- *     parameters:
- *      - name: title
- *        description: title of the book
- *        in: formData
- *        required: true
- *        type: string
- *     responses:
- *       201:
- *         description: Created
- */
-app.post('/books', (req, res) => {
-    res.status(201).send();
-});
-
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
--- src/swagger.js
+++ src/swagger.js
@@ -1,6 +1,6 @@
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs')
-const swaggerDocument = YAML.load('./src/swagger.yaml');
+const swaggerDocument = YAML.load('../src/swagger.yaml');
 
 module.exports = {
     serve: swaggerUi.serve,
